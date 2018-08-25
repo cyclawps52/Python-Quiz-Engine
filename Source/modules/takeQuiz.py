2255904cc7d1cd8536ae7a36a3900fa5d465408b
Tristan Fletcher <tristanfletcher52@gmail.com>
2018-08-24 22:14:44 -0500

    Added menu logic

--- Source/modules/takeQuiz.py
+++ Source/modules/takeQuiz.py
@@ -48,4 +48,67 @@ def quizMenu(carryID, carryClass):
         except:
             print("Invalid selection, press ENTER to retry.")
             input()
-        
+
+    # get next function call from selection
+    selectedQuiz = quizFolders[selection-1]
+    resultPath = Path(quizPath + "\\" + selectedQuiz + "\\results\\" + carryID + ".result")
+    gradePath = Path(quizPath + "\\" + selectedQuiz + "\\grades\\" + carryID + ".grade")
+    selectedQuizPath = Path(quizPath + "\\" + selectedQuiz + "\\" + selectedQuiz + ".quizfile")
+    selectedQuizDump = Path(quizPath + "\\" + selectedQuiz + "\\" + selectedQuiz + ".quizdump")
+
+    clear()
+
+    # graded quiz menu
+    try:
+        open(gradePath, "r")
+        # see result or quiz dump
+        while True:
+            print("1. See result for {0}".format(selectedQuiz))
+            print("2. See quiz dump for {0}".format(selectedQuiz))
+            selection = input("Selection: ")
+            try:
+                selection = int(selection)
+            except:
+                print("Invalid selection, press ENTER to retry.")
+                input()
+            if(selection == 1):
+                viewer(resultPath)
+                return
+            elif(selection == 2):
+                # view dump if it exists
+                try:
+                    open(selectedQuizDump, "r")
+                    viewer(selectedQuizDump)
+                    return
+                except:
+                    print("Your teacher has locked the quiz dump.")
+                    print("Press ENTER to exit the quiz menu.")
+                    input()
+                    return
+            else:
+                print("Invalid selection, press ENTER to retry.")
+                input()
+
+        return
+
+    # taken but not graded menu
+    except:
+        try:
+            open(resultPath, "r")
+            print("Your teacher has not graded " + selectedQuiz + ".")
+            print("Press ENTER to exit quiz menu.")
+            input()
+            return
+
+        # not taken, proceed to quiz engine
+        except:
+            takeQuiz(selectedQuizPath)
+            return
+
+def viewer(filePath):
+    print("VIEWING THINGY")
+    return
+
+def takeQuiz(filePath):
+    print("TAKING THINGY")
+    return
